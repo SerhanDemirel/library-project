b9690bc755a91bbbcab093f3525dccae7cf144c1
Serhan Demirel <102233776+SerhanDemirel@users.noreply.github.com>
2023-11-13 19:14:08 +0300

    library

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from "../Navbar/Navbar";
+import SearchForm from "../SearchForm/SearchForm";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className='holder'>
+        <header className='header'>
+            <Navbar />
+            <div className='header-content flex flex-c text-center text-white'>
+                <h2 className='header-title text-capitalize text-black'>find your book of choice.</h2><br />
+                <p className='header-text fs-18 fw-3 '>You can search for the book titles you want to search.</p>
+                <SearchForm />
+            </div>
+        </header>
+    </div>
+  )
+}
+
+export default Header
